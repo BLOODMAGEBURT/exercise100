83d674facc6f01071eb1ba741c4188e9dfaf8e76
burt <henghenghalala@163.com>
2018-11-05 23:21:14 +0800

    对list进行排序

--- exercise/exercise5.py
+++ exercise/exercise5.py
@@ -1,3 +1,24 @@
 # -*- coding: utf-8 -*-
 
 
+def sort_method(alist):
+    """
+    给一个list排序，从小到大
+    :param alist:
+    :return:
+    """
+    assert isinstance(alist, list)
+    new_list = sorted(alist)
+    return new_list
+
+
+if __name__ == '__main__':
+    print sort_method([2, 7, 5, 10, 4])
+
+    """
+    按学生年龄大小排队，从小到大
+    """
+    students = [('burt', 20), ('tom', 23), ('jean', 21), ('jack', 19)]
+    sorted_students = sorted(students, key=lambda x: x[1])
+    print 'sorted_students:', sorted_students
+
