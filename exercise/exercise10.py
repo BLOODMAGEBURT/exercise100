56b671a4b5245db488243163ba52f15c0b758189
burt <henghenghalala@163.com>
2018-11-07 11:48:55 +0800

    格式化时间

---
+++ exercise/exercise10.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+-------------------------------------------------
+   File Name：     exercise10
+   Description :    just like exercise9,please look at exercise9 ,thank you
+   Author :       burt
+   date：          2018/11/7
+-------------------------------------------------
+   Change Activity:
+                   2018/11/7:
+-------------------------------------------------
+    格式化当前时间 time.strftime('%Y-%m-%d %H:%M:%S',tmp)
+"""
